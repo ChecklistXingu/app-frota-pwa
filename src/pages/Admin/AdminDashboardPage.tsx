22fb14c58f1b99d17b16b497ca61b71c05f72c8f
Democrates Souza - Xingu Maquinas <xingumaquinasadm@gmail.com>
2025-12-16 16:32:46 -0300

    Ajuste no layout do dashboard para exibir o card 'Maior Consumo'

--- src/pages/Admin/AdminDashboardPage.tsx
+++ src/pages/Admin/AdminDashboardPage.tsx
@@ -192,7 +192,7 @@ const AdminDashboardPage = () => {
         </div>
       </div>
 
-      <div className="grid gap-4 lg:grid-cols-2 xl:grid-cols-4">
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         {cards.map((card) => (
           <SummaryCard
             key={card.title}
